0270b1785011c8506ac55fbb6b6f9216e06005cd
Tarun45GitHub <td2545@it.jgec.ac.in>
2024-06-29 16:16:10 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-
+import { useEffect, useState } from 'react';
 import './App.css'
 import conf from './config/config.js';
+import { useDispatch } from 'react-redux';
+import authservice from './appwrite/auth.js';
+import {login,logout} from "./store/authslice.js"
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
-  console.log(conf.appwriteBucketId);
-  return (
-    <>
-     
-    </>
-  )
+const[loading,setLoading]=useState(true);
+const dispatch=useDispatch();
+useEffect(()=>{
+  authservice.getCurrentUser()
+  .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>setLoading(false))
+},[])
+
+return !loading ? (
+  <div className='min-h-screen flex flex-wrap  content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+       todo: {/*outlate*/}
+      </main>
+      <Footer/>
+    </div>
+  </div>
+  ) : null
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from "./container/container.jsx";
+import Logo from "./Logo.jsx";
+import Logoutbtn from "./Header/Logoutbtn.jsx"
+
+export{Header,
+    Footer,
+    Container,
+    Logo,
+    Logoutbtn};
